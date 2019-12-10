e2f8c4823548b35c018e1036751c0f75b2f0311a
Maksim <maksim-popov-1996@mail.ru>
2019-12-10 20:33:27 +0300

    Gulp

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp'),
+    pug = require('gulp-pug'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    imagemin = require('gulp-tinypng');
+
+gulp.task('pug', function () {
+    return gulp.src('src/pug/pages/*.pug')
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('sass', function () {
+    return gulp.src('./src/static/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('build/css'));
+ });
+
+ gulp.task('tinypng', function () {
+     return gulp.src('src/static/img/*')
+     .pipe(imagemin('0WqvRbFmqJHNq6Dz91HjxYPtyyWZpLgR'))
+     .pipe(gulp.dest('build/img'));
+ })
+
+ gulp.task('watch', function () {
+    gulp.watch('src/pug/**/*.pug', gulp.series('pug'))
+    gulp.watch('src/static/sass/**/*.scss', gulp.series('sass'));
+ });
+
+ gulp.task('default', gulp.series(
+     gulp.parallel('pug','sass','tinypng'),
+     gulp.parallel('watch')
+     ));
